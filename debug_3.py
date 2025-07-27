d225509731c9aaa5c16dd34a76393b45504d5902
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 00:00:53 +0100

    Create debug_3.py

---
+++ debug_3.py
@@ -0,0 +1,34 @@
+# Debug 3 🐞
+
+import time
+
+# This program tells a joke and shows an alligator!
+
+# Print blank line
+print('\n')
+
+# Joke opening line – this line is missing quotation marks
+print(Let me tell you a joke)
+
+# Joke setup – missing brackets
+print'What do you call an alligator in a vest? '
+
+# Pause for dramatic effect
+time.sleep(3);
+
+# Print blank line again
+print('\n');
+
+# Joke punchline – invalid syntax and punctuation
+print(,An investigator!,)
+
+# Alligator ASCII art 🐊
+print("            .-._   _ _ _ _ _ _ _ _")
+print(" .-''-.__.-'00  '-' ' ' ' ' ' ' ' '-.")
+print("'.___ '    .   .--_'-' '-' '-' _'-' '._")
+print(" V: V 'vv-'   '_   '.       .'  _..' '.'.")
+print("   '=.____.=_.--'   :_.__.__:_   '.   : :")
+print("          (((____.-'        '-.  /   : :")
+print("                            (((-' .' /'")
+print("                          _____..'  .'")
+print("                         '-._____.-'")
