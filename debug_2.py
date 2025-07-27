e757f8c6d17e4a4b2d2d3eacad573615f6ac4e22
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-27 23:59:12 +0100

    Create debug_2.py

---
+++ debug_2.py
@@ -0,0 +1,20 @@
+# Debug 2 🐞
+
+import time
+
+# This program should tell a short joke with pauses
+
+# Prints a blank line
+Print('\n')
+
+# Joke opening line
+Print('Why do eggs not like jokes?')
+
+# Pauses the program for 3 seconds
+time.sleep(3)
+
+# Prints another blank line
+Print('\n')
+
+# Joke punchline
+Print('It always cracks them up!')
